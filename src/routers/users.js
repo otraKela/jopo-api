85029fcb7bb8c0fc21ad06aefdf0f012d6e50f40
otraKela <bustamante.marce@gmail.com>
2023-06-14 17:50:45 -0300

    agregué headers para cors

--- src/routers/users.js
+++ src/routers/users.js
@@ -6,6 +6,11 @@ const imageUploadMiddleware = require ('../middleware/imageUploadMiddleware.js')
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 
+router.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 
 router.get ( '/', usersController.list );
 router.get ( '/:id', usersController.detail );
