82f9252786e09d54a676286642f1bbba4ee670b3
otraKela <bustamante.marce@gmail.com>
2023-06-14 19:32:32 -0300

    pruebas de conexión cors

--- src/routers/users.js
+++ src/routers/users.js
@@ -1,19 +1,31 @@
 const express = require ('express');
 const router = express.Router();
+const cors = require('cors');
 
 const usersController = require ('../controllers/usersController');
 const imageUploadMiddleware = require ('../middleware/imageUploadMiddleware.js');
 
 const nodeEnv = process.env.NODE_ENV || 'development';
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200 
+}
+
+
 router.get ( '/', usersController.list );
 router.get ( '/:id', usersController.detail );
 router.post ( '/login', usersController.login );
 
 if (nodeEnv === "development") {
-  router.post ( '/registration', imageUploadMiddleware.single('image'), usersController.registration );
+  router.post ( '/registration', 
+                imageUploadMiddleware.single('image'), 
+                cors(corsOptions),
+                usersController.registration );
 } else {
-  router.post ( '/registration', usersController.registration );
+  router.post ( '/registration', 
+                cors(corsOptions),
+                usersController.registration );
 }
 
 
