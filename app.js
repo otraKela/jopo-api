7c7f73d365818de679f35a53aab748a82fd787b3
otraKela <bustamante.marce@gmail.com>
2023-06-17 12:44:47 -0300

    pruebas cors

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ app.use(express.json());
 //Aquí estoy disponiendo la posibilidad para utilizar el seteo en los formularios para el usod e los metodos put ó delete
 app.use(methodOverride('_method'));
 
-const whitelist = ['http://localhost:3000', 'https://jopo-react.netlify.app/']
+const whitelist = ['http://localhost:3000', 'https://jopo-react.netlify.app']
 
 app.use(cors(whitelist));
 
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'https://jopo-react.netlify.app'); // Permitir acceso desde cualquier origen
-//   res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE'); // Métodos permitidos
-//   res.header('Access-Control-Allow-Headers', 'Content-Type'); // Encabezados permitidos
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Permitir acceso desde cualquier origen
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE'); // Métodos permitidos
+  res.header('Access-Control-Allow-Headers', 'Content-Type'); // Encabezados permitidos
+  next();
+});
 
 const productsApiRouter = require('./src/routers/products.js');;
 const usersApiRouter= require('./src/routers/users.js')
